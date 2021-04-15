770bdef97deed50c54a4cfe751be73aebadcec41
kjezic <karlo.jezic@minus5.hr>
2021-04-15 22:26:24 +0200

    its working

--- src/main/java/hr/fer/iot/lab2/mqtt/Sensor.java
+++ src/main/java/hr/fer/iot/lab2/mqtt/Sensor.java
@@ -1,4 +1,23 @@
 package hr.fer.iot.lab2.mqtt;
 
 public class Sensor {
+
+    private static final String TEMP_PACKAGE = "<=>€##408521538#node_01#0#TCA:26.13#";
+    private static final String HUM_PACKAGE = "<=>€##408521538#node_01#0#HUMA:48#";
+
+    private double temperature;
+    private double humidity;
+
+    public Sensor() {
+        this.temperature = Double.parseDouble(TEMP_PACKAGE.split("#")[5].split(":")[1]);
+        this.humidity = Double.parseDouble(HUM_PACKAGE.split("#")[5].split(":")[1]);
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
 }
---
+++ src/main/java/hr/fer/iot/lab2/mqtt/TempHumPublisher.java
@@ -0,0 +1,60 @@
+package hr.fer.iot.lab2.mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.nio.charset.StandardCharsets;
+
+public class TempHumPublisher {
+    private static final String BROKER = "tcp://localhost:1883";
+    private static final String CLIENT_ID = "TempHumPublisher";
+    private static final int QOS = 2;
+    private static final String TEMP_TOPIC = "sensor/temperature";
+    private static final String HUM_TOPIC = "sensor/humidity";
+
+    public static void main(String[] args) {
+        MemoryPersistence persistence = new MemoryPersistence();
+        try {
+            MqttClient client = new MqttClient(BROKER, CLIENT_ID, persistence);
+
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+
+            System.out.println("Connecting to " + BROKER);
+            client.connect(options);
+            System.out.println("Connected");
+
+            Sensor sensor = new Sensor();
+
+            MqttMessage humidityMessage =
+                    new MqttMessage(String.valueOf(sensor.getHumidity()).getBytes(StandardCharsets.UTF_8));
+            humidityMessage.setQos(QOS);
+
+            MqttMessage temperatureMessage =
+                    new MqttMessage(String.valueOf(sensor.getTemperature()).getBytes(StandardCharsets.UTF_8));
+            temperatureMessage.setQos(QOS);
+
+            System.out.println("Publish humidity");
+            client.publish(HUM_TOPIC, humidityMessage);
+            System.out.println("Published humidity");
+
+            System.out.println("Publish temperature");
+            client.publish(TEMP_TOPIC, temperatureMessage);
+            System.out.println("Published temperature");
+
+            client.disconnect();
+            System.out.println("Disconnected");
+            System.exit(0);
+        } catch (MqttException e) {
+            System.out.println("reason " + e.getReasonCode());
+            System.out.println("msg " + e.getMessage());
+            System.out.println("loc " + e.getLocalizedMessage());
+            System.out.println("cause " + e.getCause());
+            System.out.println("excep " + e);
+            e.printStackTrace();
+        }
+    }
+}
